6d86227ebcb86c5ea50b5bfe95fc26ed73f8becc
Angel-Karasu <angel.karasu@proton.me>
2024-03-14 22:56:54 +0100

    Add icons and update js

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ window.onload = () => {
     img_upload_file = document.querySelector('#img-upload-file');
     img_upload_url = document.querySelector('#img-upload-url');
     
-    ['change', 'drop'].forEach(event => img_upload_file.addEventListener(event, () => load_img_file()));
-    img_upload_url.addEventListener('change', () => load_img_url());
+    ['change', 'drop'].forEach(event => img_upload_file.addEventListener(event, load_img_file));
+    img_upload_url.addEventListener('change', load_img_url);
     
     load_img_file();
     load_img_url();
